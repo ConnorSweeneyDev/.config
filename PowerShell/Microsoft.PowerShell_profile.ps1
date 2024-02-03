22dd4111f80cbf68a2456d1bdce37efc4c919eca
Connor Sweeney <75945279+ConnorSweeneyDev@users.noreply.github.com>
2024-02-03 03:08:27 +0000

    Create Microsoft.PowerShell_profile.ps1

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,20 @@
+function cw {
+    cd C:\
+    cd (C:\which\which.bat)
+}
+
+function ew {
+    cd C:\
+    explorer.exe (C:\which\which.bat)
+}
+
+function nw {
+    cd C:\
+    cd (C:\which\which.bat)
+    nvim .
+}
+
+function attend {
+    cd C:\Users\conno\Documents\Programming\Web\AutoLogAttendance
+    C:\Users\conno\Documents\Programming\Web\AutoLogAttendance\run.bat -nowin
+}
