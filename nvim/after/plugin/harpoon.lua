8a5e8d3eba0a31a937a0a8d19e579494e016458d
Connor Sweeney <75945279+ConnorSweeneyDev@users.noreply.github.com>
2024-02-03 02:53:18 +0000

    Create harpoon.lua

---
+++ nvim/after/plugin/harpoon.lua
@@ -0,0 +1,27 @@
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup({
+    settings = {
+        save_on_toggle = true
+    }
+})
+-- REQUIRED
+
+vim.keymap.set("n", "<C-e>",
+    function() harpoon.ui:toggle_quick_menu(
+        harpoon:list(),
+        {
+            title = " Harpoon ",
+            title_pos = "center",
+            border = "rounded",
+        }
+    ) end
+)
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>z", function() harpoon:list():remove() end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-t>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-g>", function() harpoon:list():next() end)
