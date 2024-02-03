395e686e01591f86684ef60181d0c046204d7d57
Connor Sweeney <75945279+ConnorSweeneyDev@users.noreply.github.com>
2024-02-03 02:56:09 +0000

    Create remap.lua

---
+++ nvim/lua/connor/remap.lua
@@ -0,0 +1,41 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<C-f>", ":!pwsh -Command \"wt --window 0 -p \"PowerShell\" pwsh -c \"nw\"\"<CR>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({"n", "v"}, "<leader>c", [["_c]])
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
+vim.keymap.set("n", "<leader>b", function()
+    vim.cmd("!build.bat")
+end)
+vim.keymap.set("n", "<leader>v", function()
+    vim.cmd("!debug.bat")
+end)
+vim.keymap.set("n", "<leader>n", function()
+    vim.cmd("!run.bat")
+end)
+
+vim.keymap.set("i", "<C-h>", "<Esc>$i<Right>();<Left><Left>")
+vim.keymap.set("i", "<C-j>", "<Esc>$i<Right>()<Left>")
+vim.keymap.set("i", "<C-k>", "<Esc>$i<Right><CR>{<CR><CR>}<Up>a<Esc>v=$i<Del>")
+vim.keymap.set("i", "<C-l>", "<Esc>$i<Right><CR>{<CR><CR>};<Up>a<Esc>v=$i<Del>")
