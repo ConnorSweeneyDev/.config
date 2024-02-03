313f6bcb8d97f1b7e1dd10a7b77c394fc983fb06
Connor Sweeney <75945279+ConnorSweeneyDev@users.noreply.github.com>
2024-02-03 05:18:58 +0000

    Update Microsoft.PowerShell_profile.ps1

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,7 @@
+function q {
+    exit
+}
+
 function cw {
     cd C:\
     cd (C:\which\which.bat)
@@ -17,4 +21,10 @@ function nw {
 function attend {
     cd C:\Users\conno\Documents\Programming\Web\AutoLogAttendance
     C:\Users\conno\Documents\Programming\Web\AutoLogAttendance\run.bat -nowin
+}
+
+function prompt {
+    $p = Split-Path -leaf -path (Get-Location)
+    $Host.UI.RawUI.WindowTitle = "$p"
+    "$pwd> "
 }
