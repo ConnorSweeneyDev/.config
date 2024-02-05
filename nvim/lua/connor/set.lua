8b483e35307426eae7233a5fb20ab1f783a73da5
Connor Sweeney <75945279+ConnorSweeneyDev@users.noreply.github.com>
2024-02-05 03:35:07 +0000

    Update set.lua

--- nvim/lua/connor/set.lua
+++ nvim/lua/connor/set.lua
@@ -1,16 +1,20 @@
+vim.g.netrw_banner = 0
+
 vim.opt.shell = "cmd.exe"
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "0"
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
+vim.opt.scrolloff = 8
 
 vim.opt.swapfile = false
 vim.opt.backup = false
@@ -21,10 +25,5 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
-
 vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = "0"
