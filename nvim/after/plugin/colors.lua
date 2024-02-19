c74d5fde36ee18016ff8a8212221bef8704849a5
Connor Sweeney <75945279+ConnorSweeneyDev@users.noreply.github.com>
2024-02-19 16:28:39 +0000

    Update colors.lua

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,9 +1,13 @@
-function ColorMyPencils(color)
-	color = color or "onedark"
+function SetColors(color)
+	color = color or "catppuccin"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+    vim.cmd("highlight @variable guifg=#D08282")
+    vim.cmd("highlight @lsp.type.variable guifg=#D08282")
+    vim.cmd("highlight Conditional cterm=NONE gui=NONE")
 end
 
-ColorMyPencils()
+SetColors()
