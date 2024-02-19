e0d95744b0b1e02224ab5a5b9e19090059278173
Connor Sweeney <75945279+ConnorSweeneyDev@users.noreply.github.com>
2024-02-19 17:59:55 +0000

    Update colors.lua

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,13 +1,12 @@
 function SetColors(color)
-	color = color or "catppuccin"
+	color = color or "onedark"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
-    vim.cmd("highlight @variable guifg=#eba0ac")
-    vim.cmd("highlight @lsp.type.variable guifg=#eba0ac")
-    vim.cmd("highlight Conditional cterm=NONE gui=NONE")
+    vim.cmd("highlight @punctuation.bracket guifg=#999999")
+    vim.cmd("highlight @punctuation.delimiter guifg=#999999")
 end
 
 SetColors()
