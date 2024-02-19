f3dddbed292adcb1a4aacddb96aac27eba9b71e2
Connor Sweeney <75945279+ConnorSweeneyDev@users.noreply.github.com>
2024-02-19 01:04:22 +0000

    Create buffers.lua

---
+++ nvim/after/plugin/buffers.lua
@@ -0,0 +1,15 @@
+function OpenBuffers()
+  local file_extensions = {'*.cpp', '*.hpp'}
+  local original_buffer = vim.api.nvim_get_current_buf()
+
+  for _, ext in ipairs(file_extensions) do
+    local files = vim.fn.globpath(vim.fn.getcwd() .. '/prog', '**/' .. ext, 0, 1)
+    for _, file in ipairs(files) do
+      vim.cmd('edit ' .. file)
+    end
+  end
+
+  vim.api.nvim_set_current_buf(original_buffer)
+end
+
+OpenBuffers()
