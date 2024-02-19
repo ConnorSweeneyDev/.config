e9e3eedd5a1f888b40509cb9fc917adfa27b4bf9
Connor Sweeney <75945279+ConnorSweeneyDev@users.noreply.github.com>
2024-02-19 16:29:20 +0000

    Update colors.lua

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,9 +1,9 @@
 function SetColors(color)
-	color = color or "catppuccin"
-	vim.cmd.colorscheme(color)
+    color = color or "catppuccin"	
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
     vim.cmd("highlight @variable guifg=#D08282")
     vim.cmd("highlight @lsp.type.variable guifg=#D08282")
