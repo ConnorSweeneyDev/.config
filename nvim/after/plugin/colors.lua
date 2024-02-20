3a3747ef77021a1a4a42e1cc463554d3d2abce35
Connor Sweeney <75945279+ConnorSweeneyDev@users.noreply.github.com>
2024-02-20 14:19:42 +0000

    Update colors.lua

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,12 +1,9 @@
 function SetColors(color)
-	color = color or "onedark"
+	color = color or "vscode"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
-    vim.cmd("highlight @punctuation.bracket guifg=#999999")
-    vim.cmd("highlight @punctuation.delimiter guifg=#999999")
 end
 
 SetColors()
