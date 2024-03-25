1f747b987aaf807c85d368be8d7bfce6ee1daf47
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-03-25 09:01:35 +0000

    Formatting

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -2,8 +2,8 @@ function SetColors(color)
 	color = color or "vscode"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
     vim.cmd("highlight Directory guibg=NONE")
     vim.cmd("highlight Question guibg=NONE")
