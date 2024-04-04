b428bf515399e0a95408f42db11e4ee9d790487b
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-04-05 00:59:17 +0100

    Google search bar now has default behaviour upon startup

--- scripts/firefox/firefoxfocusfix.bat
+++ scripts/firefox/firefoxfocusfix.bat
@@ -1,7 +1,5 @@
 @echo off
 start "" "C:\Program Files\Mozilla Firefox\firefox.exe"
 timeout /t 2 /nobreak >nul
-nircmd sendkeypress tab
-nircmd sendkeypress tab
-nircmd sendkeypress tab
-nircmd sendkeypress tab
+nircmd sendkeypress tab tab tab tab
+nircmd sendkeypress r
