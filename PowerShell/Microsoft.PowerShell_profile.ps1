05a1f9e86031dc7ab12e707deea50bc3c2df07fb
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-04-09 20:38:42 +0100

    Removed autogenerated text

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -65,8 +65,3 @@ function prompt
     $Host.UI.RawUI.WindowTitle = "$p"
     "$pwd> "
 }
-
-#34de4b3d-13a8-4540-b76d-b9e8d3851756 PowerToys CommandNotFound module
-
-Import-Module "C:\Program Files\PowerToys\WinUI3Apps\..\WinGetCommandNotFound.psd1"
-#34de4b3d-13a8-4540-b76d-b9e8d3851756
