313152b2ebceb5b26fb2bdbb707a6d4025c9051c
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-04-11 01:45:29 +0100

    Added a separator between context and content

--- nvim/after/plugin/context.lua
+++ nvim/after/plugin/context.lua
@@ -1,3 +1,16 @@
+require'treesitter-context'.setup{
+    enable = true,
+    max_lines = 0,
+    min_window_height = 0,
+    line_numbers = true,
+    multiline_threshold = 5,
+    trim_scope = 'outer',
+    mode = 'cursor',
+    separator = '-',
+    zindex = 20,
+    on_attach = nil,
+}
+
 vim.keymap.set("n", "[c", function()
   require("treesitter-context").go_to_context()
 end, { silent = true })
