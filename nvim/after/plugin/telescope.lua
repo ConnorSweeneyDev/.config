481f8713290599234c34a130905213498f2f60c7
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-04-12 20:29:31 +0100

    Better telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,13 +1,12 @@
 require('telescope').setup{
     defaults = {
-        file_ignore_patterns = { "src", "assets", ".dll", ".class", ".jar", ".sln", ".vcxproj", "packer_compiled", ".png", ".jpg", ".pyc" }
+        -- Ignore any string that has the following patterns
+        file_ignore_patterns = { ".git\\", "src\\", "assets\\", ".dll", ".class", ".jar", ".sln", ".vcxproj", "packer_compiled", ".png", ".jpg", ".pyc" }
     }
 }
-
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", default_opts)
-vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+
+vim.keymap.set('n', '<leader>pf', function() builtin.find_files({ find_command = {'rg', '--files', '--hidden'} }) end, default_opts)
+vim.keymap.set('n', '<leader>ps', function() builtin.grep_string({ search = vim.fn.input("Grep > ") }); end)
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -2,7 +2,7 @@ require'nvim-treesitter.install'.prefer_git = false
 require'nvim-treesitter.install'.compilers = { "clang" }
 
 require'nvim-treesitter.configs'.setup {
-    -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    -- A list of parser names, or "all"
     ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "hurl", "http", "html", "javascript", "css", "java", "c_sharp",
                          "cpp", "python", "sql", "xml", "json", "glsl", "cmake", "gitcommit", "gitignore", "diff", "markdown" },
 
