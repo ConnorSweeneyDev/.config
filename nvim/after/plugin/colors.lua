79d9699d439ebdae72ece0f8094d634f02e1a570
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-04-12 20:02:28 +0100

    Added comments

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,10 +1,10 @@
 function SetColors(color)
 	color = color or "vscode"
 	vim.cmd.colorscheme(color)
-
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
+    -- Specific to vscode color scheme
     vim.cmd("highlight Directory guibg=NONE")
     vim.cmd("highlight Question guibg=NONE")
     vim.cmd("highlight Conceal guibg=NONE")
@@ -14,7 +14,6 @@ function SetColors(color)
     vim.cmd("highlight MoreMsg guibg=NONE")
     vim.cmd("highlight ModeMsg guibg=NONE")
     vim.cmd("highlight FoldColumn guibg=NONE")
-
     vim.cmd("highlight DiffAdd guibg=NONE guifg=#4b5632")
     vim.cmd("highlight DiffChange guibg=NONE guifg=NONE")
     vim.cmd("highlight DiffDelete guibg=NONE guifg=#6f1313")
