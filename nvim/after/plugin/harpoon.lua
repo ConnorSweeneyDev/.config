fab10ce590ee1501c20d76fbd9013f35ff488a7a
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-04-14 16:05:44 +0100

    Updates

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -19,7 +19,7 @@ vim.keymap.set("n", "<C-e>",
     ) end
 )
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<leader>z", function() harpoon:list():remove() end)
 vim.keymap.set("n", "<C-z>", function() harpoon:list():clear() end)
 
--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -23,3 +23,5 @@ require'nvim-treesitter.configs'.setup {
         additional_vim_regex_highlighting = false,
     },
 }
+
+vim.treesitter.language.register('cpp', 'tpp')
