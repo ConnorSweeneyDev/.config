45932e9165c7f0479a02501b0c4c350f067b316c
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-04-17 00:08:55 +0100

    Undo previous commit

--- scripts/firefox/firefoxfocusfix.bat
+++ scripts/firefox/firefoxfocusfix.bat
@@ -3,5 +3,3 @@ start "" "C:\Program Files\Mozilla Firefox\firefox.exe"
 timeout /t 2 /nobreak >nul
 nircmd sendkeypress tab tab tab tab
 nircmd sendkeypress r
-timeout /t 1 /nobreak >nul
-nircmd sendkeypress esc
