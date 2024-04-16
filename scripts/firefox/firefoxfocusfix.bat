52eaa13beae640e6f2cd8d75d5cec2a2229c3148
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-04-17 00:04:28 +0100

    Esc pressed after refreshing

--- scripts/firefox/firefoxfocusfix.bat
+++ scripts/firefox/firefoxfocusfix.bat
@@ -3,3 +3,5 @@ start "" "C:\Program Files\Mozilla Firefox\firefox.exe"
 timeout /t 2 /nobreak >nul
 nircmd sendkeypress tab tab tab tab
 nircmd sendkeypress r
+timeout /t 1 /nobreak >nul
+nircmd sendkeypress esc
