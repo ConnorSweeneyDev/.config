96ab2095eeeef7f39cb20ae9f62259b9b999b95c
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-04-17 13:36:34 +0100

    Added bindings for * searching

--- nvim/lua/connor/remap.lua
+++ nvim/lua/connor/remap.lua
@@ -13,6 +13,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "*", "*zzzv")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
