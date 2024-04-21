eaffb3b6d25308667d69289100cb08757de7cce9
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-04-21 23:22:33 +0100

    Removed extra deletion of brackets on backspace press

--- nvim/after/plugin/autopairs.lua
+++ nvim/after/plugin/autopairs.lua
@@ -3,5 +3,6 @@ require("nvim-autopairs").setup
     fast_wrap = { map = "<C-b>", cursor_pos_before = false },
     disable_in_macro = false,
     disable_in_visualblock = false,
-    enable_moveright = false
+    enable_moveright = false,
+    map_bs = false
 }
