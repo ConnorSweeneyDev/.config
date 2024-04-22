4e10cdda6727f8484bd1b7193bca1e70de562b64
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-04-22 16:20:57 +0100

    Removed unreliable launch option from FZF call

--- scripts/which/which.bat
+++ scripts/which/which.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-fzf --border --info=inline --preview "bat --style=numbers --color=always {}" --preview-window "~3" > "C:\scripts\which\path.txt"
+fzf --border --info=inline --preview "bat --style=numbers --color=always {}" > "C:\scripts\which\path.txt"
 
 python C:\scripts\which\folder.py
 
