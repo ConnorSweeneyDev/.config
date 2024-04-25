00885644751c5455a65837f205eaa9bcc6abaa29
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-04-25 20:30:48 +0100

    Added git commands to help with merge conflicts

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,1 +1,4 @@
-vim.keymap.set("n", "<LEADER>gs", "<CMD>Git<CR>")
+vim.keymap.set("n", "<LEADER>gs", "<CMD>G<CR>")
+vim.keymap.set("n", "<LEADER>gd", "<CMD>Gvdiffsplit!<CR>")
+vim.keymap.set("n", "<LEADER>gc", "<CMD>G commit<CR>")
+vim.keymap.set("n", "<LEADER>gg", ":diffget ")
