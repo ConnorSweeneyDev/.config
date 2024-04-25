b2ab253335995814f1a68b4e79cc9d30505ff16e
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-04-25 20:56:52 +0100

    Less annoying notifications

--- nvim/after/plugin/notify.lua
+++ nvim/after/plugin/notify.lua
@@ -1,3 +1,3 @@
-require("notify").setup{ timeout = 1000, background_colour = "#000000" }
+require("notify").setup{ timeout = 1000, background_colour = "#000000", fps = 60, render = "minimal", stages = "fade" }
 
 vim.keymap.set("n", "<LEADER>pm", "<CMD>Telescope notify<CR>")
