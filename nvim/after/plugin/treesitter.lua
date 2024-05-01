5aacf0c83c1cf6e733621a004858f5296ab69d6c
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-05-01 17:13:53 +0100

    Better formatting

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -9,10 +9,5 @@ require"nvim-treesitter.configs".setup
 
     sync_install = false,
     auto_install = false,
-
-    highlight =
-    {
-        enable = true,
-        additional_vim_regex_highlighting = false
-    }
+    highlight = { enable = true, additional_vim_regex_highlighting = false }
 }
