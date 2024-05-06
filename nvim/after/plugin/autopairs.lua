dac395296a8ef94d758f7cc34c28183627ec7ae3
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-05-06 01:19:49 +0100

    Better scrolloff and removed unused lua file

--- nvim/after/plugin/autopairs.lua
+++
@@ -1,8 +0,0 @@
-require("nvim-autopairs").setup
-{
-    fast_wrap = { map = "<C-b>", cursor_pos_before = false },
-    disable_in_macro = false,
-    disable_in_visualblock = false,
-    enable_moveright = false,
-    map_bs = false
-}
--- nvim/lua/connor/set.lua
+++ nvim/lua/connor/set.lua
@@ -14,7 +14,7 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 20
 
 vim.opt.swapfile = false
 vim.opt.undofile = true
