372035b710ef9c2a3a2ab217ed5a30c5e07bd455
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-06-06 09:01:09 +0100

    Tabs changed to 2 spaces rather than 4

--- nvim/lua/connor/set.lua
+++ nvim/lua/connor/set.lua
@@ -12,9 +12,9 @@ vim.opt.relativenumber = true
 vim.opt.signcolumn = "no"
 vim.opt.colorcolumn = "100"
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
