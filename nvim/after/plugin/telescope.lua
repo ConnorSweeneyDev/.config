4d408af6c8efab9c56c30f6786b75c7965861889
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-06-18 14:19:05 +0100

    system/ -> script/

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -3,7 +3,8 @@ require("telescope").setup
   defaults =
   {
     -- Ignore any string that has the following patterns
-    file_ignore_patterns = { ".git\\", ".cache\\", "external\\", "assets\\", ".exe", ".dll", ".class", ".jar", ".sln", ".vcxproj", ".png", ".jpg", ".pyc", "packer_compiled.lua" }
+    file_ignore_patterns = { ".git\\", ".cache\\", "external\\", "assets\\", ".exe", ".dll",
+                             ".class", ".jar", ".sln", ".vcxproj", ".png", ".jpg", ".pyc", "packer_compiled.lua" }
   }
 }
 local builtin = require("telescope.builtin")
