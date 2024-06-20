b04cc5ae6f2a792593822835058fd8e3f16eb7d2
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-06-20 13:19:42 +0100

    Formatting fix

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -7,8 +7,8 @@ require("telescope").setup
                              ".class", ".jar", ".sln", ".vcxproj", ".png", ".jpg", ".pyc", "packer_compiled.lua" }
   }
 }
-local builtin = require("telescope.builtin")
 
+local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<LEADER>pf", function() builtin.find_files({ find_command = {"rg", "--files", "--hidden"} }) end, default_opts)
 vim.keymap.set("n", "<LEADER>ps", function() builtin.grep_string({ search = vim.fn.input("Search Term: "), ignorecase = false }); end)
 vim.keymap.set("n", "<LEADER>pb", builtin.buffers, {})
