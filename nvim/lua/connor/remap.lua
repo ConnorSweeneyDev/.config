fd1dafff3abe029db5bad1a2cda6b543846e0f67
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-06-23 16:53:07 +0100

    Added bind to toggle line wrapping

--- nvim/lua/connor/remap.lua
+++ nvim/lua/connor/remap.lua
@@ -32,6 +32,7 @@ vim.keymap.set("n", "<LEADER>s", [[:%s/<C-r><C-w>/<C-r><C-w>/g<Left><Left>]])
 vim.keymap.set("v", "<LEADER>s", "\"hy:%s/<C-r>h/<C-r>h/g<Left><Left>")
 vim.keymap.set("v", "<C-s>", ":s//g<Left><Left>")
 vim.keymap.set("v", "<C-n>", ":normal ")
+vim.keymap.set("n", "<LEADER>tw", "<CMD>set wrap!<CR>")
 
 vim.keymap.set("n", "<LEADER>qg", ":silent grep  program<Left><Left><Left><Left><Left><Left><Left><Left>")
 vim.keymap.set("n", "<LEADER>qr", ":cdo s//g<Left><Left>")
