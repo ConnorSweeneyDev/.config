c1abd392a4fbfae94f8bdfaa791b7fff714d3dcd
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-07-04 19:35:38 +0100

    Ansi escape sequences disabled for neovim's terminal

--- nvim/lua/connor/set.lua
+++ nvim/lua/connor/set.lua
@@ -1,7 +1,7 @@
 vim.g.netrw_banner = 0
 
 vim.opt.shell = "pwsh.exe"
-vim.opt.shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+vim.opt.shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;$PSStyle.Formatting.Error = '';$PSStyle.Formatting.ErrorAccent = '';$PSStyle.Formatting.Warning = '';$PSStyle.OutputRendering = 'PlainText';"
 vim.opt.shellredir = "2>&1 | Out-File -Encoding utf8 %s; exit $LastExitCode"
 vim.opt.shellpipe = "2>&1 | Out-File -Encoding utf8 %s; exit $LastExitCode"
 vim.opt.shellquote = ""
