afdd0b3eea04f7d56658c9c9900b8500331b15e2
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-07-19 17:51:31 +0100

    Added ability to ignore certain files

--- nvim/after/plugin/buffers.lua
+++ nvim/after/plugin/buffers.lua
@@ -2,6 +2,7 @@ function open_buffers()
   -- List of folders and file extensions to search for and open
   local folders = {"/program", "/prog", "/lua", "/after", "/script"}
   local file_extensions = {"*.cpp", "*.hpp", "*.c", "*.h", "*.glsl", "*.cs", "*.java", "*.py", "*.lua", "*.bat"}
+  local ignore_files = {"resource.hpp", "resource.cpp"}
 
   local original_buffer = vim.api.nvim_get_current_buf()
 
@@ -9,7 +10,9 @@ function open_buffers()
     for _, extension in ipairs(file_extensions) do
       local files = vim.fn.globpath(vim.fn.getcwd() .. folder, "**/" .. extension, 0, 1)
       for _, file in ipairs(files) do
-        vim.cmd("edit " .. file)
+        if not vim.tbl_contains(ignore_files, vim.fn.fnamemodify(file, ":t")) then
+          vim.cmd("edit " .. file)
+        end
       end
     end
   end
