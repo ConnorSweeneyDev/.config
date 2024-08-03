df38fbac9e7516615cfd5763c758b1081994a435
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-08-03 10:22:16 +0100

    Better line numbers and splits

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -4,7 +4,7 @@ function set_colors(color)
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
-  -- Specific to vscode color scheme
+  -- Specific to vscode color scheme with transparent background
   vim.cmd("highlight Directory guibg=NONE")
   vim.cmd("highlight Question guibg=NONE")
   vim.cmd("highlight Conceal guibg=NONE")
@@ -21,3 +21,24 @@ function set_colors(color)
 end
 
 set_colors()
+
+vim.api.nvim_create_autocmd({"BufEnter", "WinEnter"},
+{
+  callback = function()
+    local separator = " ▎ "
+    vim.opt.statuscolumn =
+    '%s%=%#LineNr4#%{(v:relnum >= 4)?v:relnum.\"' .. separator .. '\":\"\"}' ..
+    '%#LineNr3#%{(v:relnum == 3)?v:relnum.\"' .. separator .. '\":\"\"}' ..
+    '%#LineNr2#%{(v:relnum == 2)?v:relnum.\"' .. separator .. '\":\"\"}' ..
+    '%#LineNr1#%{(v:relnum == 1)?v:relnum.\"' .. separator .. '\":\"\"}' ..
+    '%#LineNr0#%{(v:relnum == 0)?v:lnum.\"' .. separator .. '\":\"\"}'
+
+    vim.cmd("highlight LineNr0 guifg=#dedede")
+    vim.cmd("highlight LineNr1 guifg=#bdbdbd")
+    vim.cmd("highlight LineNr2 guifg=#9c9c9c")
+    vim.cmd("highlight LineNr3 guifg=#7b7b7b")
+    vim.cmd("highlight LineNr4 guifg=#5a5a5a")
+
+    vim.opt.laststatus = 3
+  end
+})
