f22b431fe7aa71be5296c85ac680a7787524fd02
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-08-14 12:46:16 +0100

    Commit hash for markdown preview added

--- nvim/lua/connor/packer.lua
+++ nvim/lua/connor/packer.lua
@@ -13,18 +13,18 @@ return require("packer").startup(function(use)
   use{"itchyny/vim-qfedit", commit = "d05f2f3"}
   use{"ggandor/leap.nvim", commit = "c6bfb19"}
 
-  use("nvim-tree/nvim-web-devicons")
   use{"MunifTanjim/nui.nvim", commit = "61574ce"}
+  use{"nvim-tree/nvim-web-devicons", commit = "3722e3d"}
   use{"tribela/vim-transparent", commit = "7b34267"}
   use{"Mofiqul/vscode.nvim", as = "vscode", commit = "7de58b7"}
-  use{"folke/noice.nvim", commit = "d9328ef"}
   use{"nvim-lualine/lualine.nvim", commit = "b431d22"}
   use{"rcarriga/nvim-notify", commit = "d333b6f"}
+  use{"folke/noice.nvim", commit = "d9328ef"}
 
   use{"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
   use("nvim-treesitter/nvim-treesitter-context")
   use{"neoclide/coc.nvim", branch = "release"}
   use("tpope/vim-fugitive")
   use("github/copilot.vim")
-  use{"iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }}
+  use{"iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, commit = "a923f5f"}
 end)
