c4ca49037408d53d57c28ed2fff0003557d45bc2
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-08-14 20:51:31 +0100

    Better mapping for diffget

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,4 +1,7 @@
 vim.keymap.set("n", "<LEADER>gs", "<CMD>G<CR>")
 vim.keymap.set("n", "<LEADER>gp", "<CMD>G pull<CR>")
 vim.keymap.set("n", "<LEADER>gr", "<CMD>G restore .<CR>")
+
+vim.keymap.set("n", "<LEADER>dh", "<CMD>diffget //2<CR>")
+vim.keymap.set("n", "<LEADER>dl", "<CMD>diffget //3<CR>")
 vim.keymap.set("n", "<LEADER>dq", "<C-w>o<CMD>G<CR>")
