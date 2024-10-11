01471b496c73ef1155077de0b6ff9874f43c6e20
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-10-11 23:25:36 +0100

    Removed current domain from new tab

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -21,7 +21,7 @@ config.keys =
   { key = 'v', mods = 'LEADER | CTRL', action = wezterm.action.PasteFrom 'Clipboard' },
   { key = 'j', mods = 'LEADER | CTRL', action = wezterm.action.ScrollByPage(1) },
   { key = 'k', mods = 'LEADER | CTRL', action = wezterm.action.ScrollByPage(-1) },
-  { key = 't', mods = 'LEADER | CTRL', action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
+  { key = 't', mods = 'LEADER | CTRL', action = wezterm.action.SpawnTab},
   { key = 'w', mods = 'LEADER | CTRL', action = wezterm.action.SpawnWindow }
 }
 
