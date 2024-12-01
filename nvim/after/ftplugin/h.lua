35cb6d7c909856c6856ce0ce6872c6cd11ab0b35
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-12-01 23:34:16 +0000

    Fixed confilicting bindings

--- nvim/after/ftplugin/h.lua
+++ nvim/after/ftplugin/h.lua
@@ -1,3 +1,3 @@
-map("n", "<C-u>c", function() c_util.switch_file_in_compilation_unit("/program", "source") end)
-map("n", "<C-u>h", function() c_util.switch_file_in_compilation_unit("/program", "header") end)
+map("n", "UC", function() c_util.switch_file_in_compilation_unit("/program", "source") end)
+map("n", "UH", function() c_util.switch_file_in_compilation_unit("/program", "header") end)
 map("n", "<LEADER>kf", "<CMD>!clang-format -i %<CR>")
