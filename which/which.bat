436652d71ff5bec5cadde0d300385f64c5d50e8c
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-12-04 14:56:20 +0000

    horizontal split on fzf preview window

--- which/which.bat
+++ which/which.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-fzf --preview "bat --style=numbers --color=always {}" > "%WHICH%\path.txt"
+fzf --preview "bat --style=numbers --color=always {}" --preview-window top > "%WHICH%\path.txt"
 
 python "%WHICH%\folder.py"
 FOR /F "delims=" %%i IN (%WHICH%\path.txt) DO @ECHO %%i
