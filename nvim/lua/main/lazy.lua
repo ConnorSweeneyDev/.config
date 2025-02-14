79d84057a44fab5855422b57d0eb4f93ec97fd09
Connor Sweeney <connor.sweeney1234@gmail.com>
2025-02-14 14:57:00 +0000

    Updated coc

--- nvim/lua/main/lazy.lua
+++ nvim/lua/main/lazy.lua
@@ -20,7 +20,7 @@ require("lazy").setup{
   {"rcarriga/nvim-notify"},
   {"folke/noice.nvim"},
   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-  {"neoclide/coc.nvim", branch = "release", commit = "ae1a557"},
+  {"neoclide/coc.nvim", branch = "release", commit = "ebe7a08"},
   {"sindrets/diffview.nvim"},
   {"NeogitOrg/neogit"},
   {"supermaven-inc/supermaven-nvim"}
