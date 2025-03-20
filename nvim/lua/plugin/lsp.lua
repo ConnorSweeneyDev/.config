f909bc77f9fd6bb1ece805406980088df1cf9ffc
Connor Sweeney <connor.sweeney1234@gmail.com>
2025-03-20 22:02:14 +0000

    Cleaner lsp config

--- nvim/lua/plugin/lsp.lua
+++ nvim/lua/plugin/lsp.lua
@@ -1,21 +1,11 @@
-Lsp.handlers["textDocument/publishDiagnostics"] = Lsp.with(Lsp.diagnostic.on_publish_diagnostics, {
-  virtual_text = false,
-  signs = true,
-  update_in_insert = true,
-  underline = true,
-})
 Lsp.inlay_hint.enable(false)
 Diagnostic.config({
+  update_in_insert = true,
   severity_sort = true,
-  float = { border = "rounded", source = "if_many" },
-  underline = {
-    severity = {
-      Diagnostic.severity.ERROR,
-      Diagnostic.severity.WARN,
-      Diagnostic.severity.INFO,
-      Diagnostic.severity.HINT,
-    },
-  },
+  virtual_text = false,
+  virtual_lines = false,
+  underline = true,
+  float = { source = "if_many" },
   signs = {
     text = {
       [Diagnostic.severity.ERROR] = "▌",
@@ -29,3 +19,4 @@ Map("n", "<LEADER>cr", "<CMD>LspRestart<CR>")
 Map("n", "<LEADER>ka", Lsp.buf.code_action)
 Map("n", "<LEADER>rn", Lsp.buf.rename)
 Map("n", "K", Lsp.buf.hover)
+Map("n", "<LEADER>e", function() Diagnostic.open_float({ scope = "cursor" }) end)
