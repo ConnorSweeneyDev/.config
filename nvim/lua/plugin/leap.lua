86ae1b84d8f54401f89df79fa3b3e41bd8b40829
Connor Sweeney <connor.sweeney1234@gmail.com>
2025-07-26 05:41:46 +0100

    Better leap binding.

--- nvim/lua/plugin/leap.lua
+++ nvim/lua/plugin/leap.lua
@@ -1,1 +1,1 @@
-vim.keymap.set({ "n", "x" }, "s", function() Leap_util.leap_by_word(require("leap")) end)
+vim.keymap.set({ "n", "v", "o" }, "s", function() Leap_util.leap_by_word(require("leap")) end)
