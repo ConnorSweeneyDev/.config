20ea13d5d97d07de22a2f7ad62f9d97de9d76083
Connor Sweeney <connor.sweeney1234@gmail.com>
2026-01-15 19:39:02 +0000

    Updated for new opencode command.

--- pwsh/alias.ps1
+++ pwsh/alias.ps1
@@ -1,17 +1,4 @@
 function c { Clear-Host }
-function o { opencode . --port }
-function n { nvim . }
-function v
-{
-  $solution = Get-ChildItem -Path (Get-Location) -Filter *.sln -File -Recurse -Depth 1 | Select-Object -First 1
-  if ($solution)
-  {
-    $solutionName = $solution.FullName
-    devenv "$solutionName"
-  }
-  else { devenv . }
-}
-
 function d
 {
   $paths = $args
@@ -20,4 +7,17 @@ function d
     if (Test-Path $path) { Remove-Item $path -Recurse -Force }
     else { echo "Path does not exist: $path" }
   }
+}
+
+function n { nvim . }
+function o { $Env:OPENCODE_EXPERIMENTAL = "ALL"; opencode . --port; Remove-Item Env:OPENCODE_EXPERIMENTAL }
+function v
+{
+  $solution = Get-ChildItem -Path (Get-Location) -Filter *.sln -File -Recurse -Depth 1 | Select-Object -First 1
+    if ($solution)
+    {
+      $solutionName = $solution.FullName
+        devenv "$solutionName"
+    }
+    else { devenv . }
 }
