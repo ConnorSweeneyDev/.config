2dd0d202fa46eae1e7c74c9c8eefb61eb0984551
Connor Sweeney <connor.sweeney1234@gmail.com>
2026-03-07 18:58:21 +0000

    Better dap config.

--- nvim/lua/plugin/dap.lua
+++ nvim/lua/plugin/dap.lua
@@ -1,7 +1,7 @@
 local dap = require("dap")
-vim.keymap.set("n", "<F5>", function() dap.terminate({ hierarchy = true }) end, { desc = "DAP Terminate" })
-vim.keymap.set("n", "<F6>", dap.continue, { desc = "DAP Continue" })
-vim.keymap.set("n", "<F7>", dap.toggle_breakpoint, { desc = "DAP Toggle Breakpoint" })
+vim.keymap.set("n", "<LEADER>b", dap.toggle_breakpoint, { desc = "DAP Toggle Breakpoint" })
+vim.keymap.set("n", "<F4>", function() dap.terminate({ hierarchy = true }) end, { desc = "DAP Terminate" })
+vim.keymap.set("n", "<F5>", dap.continue, { desc = "DAP Continue" })
 vim.keymap.set("n", "<F8>", dap.step_over, { desc = "DAP Step Over" })
 vim.keymap.set("n", "<F9>", dap.step_into, { desc = "DAP Step Into" })
 vim.keymap.set("n", "<F10>", dap.step_out, { desc = "DAP Step Out" })
@@ -15,7 +15,8 @@ require("dap-view").setup({
     show = true,
     sections = { "console", "watches", "scopes", "exceptions", "breakpoints", "threads" },
     default_section = "console",
+    show_keymap_hints = false,
   },
-  windows = { size = 0.5, position = "below" },
+  windows = { size = 0.4, position = "below" },
   auto_toggle = true,
 })
