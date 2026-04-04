9324624d26a5282d617a8bd5397e9a7b4f013de6
Connor Sweeney <connor.sweeney1234@gmail.com>
2026-04-04 07:09:26 +0100

    Stabilized blink version.

--- nvim/lua/main/pack.lua
+++ nvim/lua/main/pack.lua
@@ -14,7 +14,7 @@ vim.pack.add({
   { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
   { src = "https://github.com/williamboman/mason.nvim" },
   { src = "https://github.com/neovim/nvim-lspconfig" },
-  { src = "https://github.com/saghen/blink.cmp", version = vim.version.range("*") },
+  { src = "https://github.com/saghen/blink.cmp", version = vim.version.range("1.*") },
   { src = "https://github.com/folke/trouble.nvim" },
   { src = "https://github.com/mfussenegger/nvim-dap" },
   { src = "https://github.com/igorlfs/nvim-dap-view" },
